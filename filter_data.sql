1f3c7ee9877517b32aeb9f578a386fa954eca262
Davi Antônio da Silva Santos <antoniossdavi@gmail.com>
2022-05-05 20:56:45 -0300

    Add SQL script for filtering data
    Filter invalid rows and columns from the database. Sqlite will also be
    used to export the resulting dataset to CSV.

---
+++ filter_data.sql
@@ -0,0 +1,10 @@
+SELECT
+	track_id, title, song_id, release, artist_id,
+	artist_name, duration, artist_familiarity, artist_hotttnesss, year,
+	track_7digitalid, shs_perf, shs_work
+FROM
+	songs
+WHERE
+	title IS NOT NULL AND title != ''
+	AND release IS NOT NULL AND release != ''
+	AND year IS NOT NULL AND year != 0;
